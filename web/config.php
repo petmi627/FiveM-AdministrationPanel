d6710fc12c0392bdc08268079d45b15dcfa9f432
Mike <mike@petmi627.lu>
2020-06-20 13:16:48 +0200

    Dockerized Application

--- web/config.php
+++ web/config.php
@@ -1,17 +1,16 @@
 <?php
-	
 	#region Database Settings
 	// DATABASE SETTINGS
-	$GLOBALS['mysql_host'] = "";								// MySQL Host
-	$GLOBALS['mysql_user'] = "";								// MySQL User
-	$GLOBALS['mysql_pass'] = "";								// MySQL Password
-	$GLOBALS['mysql_db'] = "";									// MySQL Database
+	$GLOBALS['mysql_host'] = $_ENV["MYSQL_HOST"];				// MySQL Host
+	$GLOBALS['mysql_user'] = $_ENV["MYSQL_USER"];				// MySQL User
+	$GLOBALS['mysql_pass'] = $_ENV["MYSQL_PASSWORD"];			// MySQL Password
+	$GLOBALS['mysql_db'] = $_ENV["MYSQL_DATABASE"];				// MySQL Database
 	#endregion
 	
 	// SITE SETTINGS
-	$GLOBALS['domainname'] = "";										// URL (and folder if used) with protocol and trailing slash. Example: https://arthurmitchell.xyz/beta/
-	$GLOBALS['subfolder'] = ""; 										// If accessing via a sub folder type the sub folder name out like the following: /foldername Example: /staff otherwise leave blank
-	$GLOBALS['apikey'] = "";								 			// SteamCommunity API Key https://steamcommunity.com/dev/apikey
-	date_default_timezone_set('America/New_York');						// Timezone (http://php.net/manual/en/timezones.php)
+	$GLOBALS['domainname'] = $_ENV["APP_URL"];					// URL (and folder if used) with protocol and trailing slash. Example: https://arthurmitchell.xyz/beta/
+	$GLOBALS['subfolder'] = $_ENV["APP_SUBFOLDER"]; 			// If accessing via a sub folder type the sub folder name out like the following: /foldername Example: /staff otherwise leave blank
+	$GLOBALS['apikey'] = $_ENV["APP_STEAM_API_KEY"];			// SteamCommunity API Key https://steamcommunity.com/dev/apikey
+	date_default_timezone_set($_ENV["TZ"]);						// Timezone (http://php.net/manual/en/timezones.php)
 
 ?>
